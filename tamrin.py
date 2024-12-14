f3ca926b8143664bb7e0f11ab26c873ff2f15079
Ahmadsiar Hamidi <ahmadsiarhamidi298@gmail.com>
2024-12-14 14:33:57 +0330

    second commit

---
+++ tamrin.py
@@ -0,0 +1,88 @@
+# x = int(input())
+# y = int(input())
+# z = int(input())
+
+# if x!=0 and y!=0 and z!=0:
+#     if x+y+z==180:
+#         print("Yes")
+#     else:
+#         print("No")
+# else:
+#     print("tarif nashoda")    
+
+
+# def calculate_total_sale(items,prices):
+#     total_sale = sum(items[i] * prices[i] for i in range(len(items)))
+#     return total_sale
+    
+# num_items = int(input("Enter the number of items: "))
+# items = []
+# prices = []
+
+# for i in range(num_items):
+#     item = int(input(f"\nEnter the quantity of item {i+1}:"))
+#     price = float(input(f"Enter the price of item {i+1}:")
+#     items.append(item)
+#     prices.append(price)
+    
+# total_sale = calculate_total_sale(items,prices)
+# print(f"The total sale amount is : {total_sale}")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        
+        
+    
+        
+    
